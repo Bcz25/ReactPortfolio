07bd56cd89b01f0e5ba4eddbd3c028b12af62513
Brett Czerwinski <brettcz@outlook.com>
2024-07-23 12:36:35 -0600

    fixed nav layout on mobile

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function Nav() {
     return (
-      <nav className=" w-full p-2 space-x-2">
-        <Link to="/" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">About Me</Link>
-        <Link to="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Projetcs</Link>
-        <Link to="/resume" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Resume</Link>
-        <Link to="/contact" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Contact</Link>
+      <nav className="flex flex-row flex-wrap justify-center items-center w-full p-2 space-x-3">
+        <Link to="/" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">About Me</Link>
+        <Link to="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Projetcs</Link>
+        <Link to="/resume" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Resume</Link>
+        <Link to="/contact" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Contact</Link>
       </nav>
     )
   }
