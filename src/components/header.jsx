d7529cca3df4ee2586bb1c0bc3a15128937eb12b
Brett Czerwinski <brettcz@outlook.com>
2024-07-23 10:58:24 -0600

    testing nelify bug fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import Nav from "./nav";
 
 function Header() {
     return (
-      <header className="flex flex-col justify-between items-start py-5 px-5 relative isolate overflow-auto bg-gray-700 sm:py-8 bg-[url('./src/assets/portfolio-background2.png')] bg-cover bg-center before:absolute before:inset-0 before:bg-gray-900 before:opacity-30 before:z-0">
+      <header className="flex header flex-col justify-between items-start py-5 px-5 relative isolate overflow-auto bg-gray-700 sm:py-8 bg-cover bg-center before:absolute before:inset-0 before:bg-gray-900 before:opacity-30 before:z-0">
         <div className="self-end relative z-10 ">
           <Nav />
         </div>
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Nav() {
     return (
       <nav className=" w-full p-2 space-x-2">
-        <a href="/" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">About Me</a>
-        <a href="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Projetcs</a>
-        <a href="/resume" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Resume</a>
-        <a href="/contact" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Contact</a>
+        <Link to="/" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">About Me</Link>
+        <Link to="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Projetcs</Link>
+        <Link to="/resume" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Resume</Link>
+        <Link to="/contact" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">Contact</Link>
       </nav>
     )
   }
