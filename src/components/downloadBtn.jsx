d0a3307925eecb1f93fa53126b196a15bc6396e4
Brett Czerwinski <brettcz@outlook.com>
2024-07-24 17:33:45 -0600

    fixed resume download

--- src/components/downloadBtn.jsx
+++ src/components/downloadBtn.jsx
@@ -1,45 +1,15 @@
-import html2canvas from 'html2canvas';
-import jsPDF from 'jspdf';
+import resumePdf from '../assets/Brett-Czerwinski-resume.pdf';
 
 const DownloadBtn = () => {
     const downloadPdf = () => {
-    const input = document.getElementById('pdfDoc');
-    html2canvas(input).then((canvas) => {
-        const imgData = canvas.toDataURL('image/png');
-        const pdf = new jsPDF({
-            orientation: 'portrait',
-            unit: 'pt',
-            format: 'a4'
-        });
-
-        // Document properties
-        pdf.setProperties({
-            title: 'Resume',
-            subject: 'A formatted version of the resume',
-            author: 'Brett Czerwinski',
-        });
-
-        // Custom font and style
-        pdf.setFont('helvetica');
-        pdf.setFontSize(12);
-        pdf.setTextColor(60);
-
-        // Add image
-        pdf.addImage(imgData, 'PNG', 40, 40, 512, 512);
-
-        // Add text with custom formatting
-        pdf.text("Brett Czerwinski's Resume", 40, 30);
-
-        // Footer
-        const pageCount = pdf.internal.getNumberOfPages();
-        for (let i = 1; i <= pageCount; i++) {
-            pdf.setPage(i);
-            pdf.text(40, pdf.internal.pageSize.height - 20, 'Page ' + String(i));
-        }
-
-        pdf.save("brett-czerwinski-resume.pdf");
-    });
-};
+        const link = document.createElement('a');
+        console.log(resumePdf)
+        link.href = resumePdf;
+        link.download = 'Brett-Czerwinski-resume.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
 return (
     <button onClick={downloadPdf} className="text-gray-900 font-bold bg-teal-700 rounded shadow p-3 hover:bg-teal-900">
         Download Resume
