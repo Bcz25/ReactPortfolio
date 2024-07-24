e822f17a025981835a059eec164488881c3b0c13
Brett Czerwinski <brettcz@outlook.com>
2024-07-23 21:03:55 -0600

    spelling fix

--- src/pages/aboutPage.jsx
+++ src/pages/aboutPage.jsx
@@ -16,7 +16,7 @@ function About() {
                 I was born and raised in South Florida where I met my beautiful wife, Xela, and we had our first child, Sky. In March of 2023, we trekked across the U.S to snowy Utah so I could pursue a job in Cyber Security. Since moving here, I've discovered a love for hiking and snowboarding. I have also discovered a profound interest in programming and am currently doing a bootcamp with the University of Utah to expand my knowledge and gain more hands-on experience in programming.
               </p>
               <p className='m-2 p-2'>
-                I am a skilled full stack web developer with experience in HTML, CSS, JavaScript, jQuery, Node.js, React, Express, NoSql/MongoDb, postreSql, and Inquirer. I have managed web content, design, and backend databases for multiple projects, ensuring responsive and accessible websites.
+                I am now a skilled full stack web developer with experience in HTML, CSS, JavaScript, jQuery, Node.js, React, Express, NoSql/MongoDb, postreSql, and Inquirer. I have managed web content, design, and backend databases for multiple projects, ensuring responsive and accessible websites.
               </p>
             </div>
           </div>
