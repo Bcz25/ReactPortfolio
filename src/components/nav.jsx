e68d61a84cc86e552e828caef5c3a7618a55d2d8
Bcz25 <160162513+Bcz25@users.noreply.github.com>
2024-07-24 19:14:10 -0600

    Update nav.jsx
    Spelling fix

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
     return (
       <nav className="flex flex-row flex-wrap justify-center items-center w-full p-2 space-x-3">
         <Link to="/" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">About Me</Link>
-        <Link to="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Projetcs</Link>
+        <Link to="/projects" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Projects</Link>
         <Link to="/resume" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Resume</Link>
         <Link to="/contact" className="text-gray-900 font-bold bg-teal-700 rounded shadow p-2 hover:bg-teal-900">Contact</Link>
       </nav>
